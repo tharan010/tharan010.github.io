26b31e967c6d5e5d6833aa306dcbb969aa0094bd
ts39233-admin <tharan.suresh@utexas.edu>
2026-07-22 12:58:59 -0500

    'design update'

---
+++ analytics.js
@@ -0,0 +1,19 @@
+// Google Analytics 4 (free). Get your Measurement ID at analytics.google.com
+// (Admin > Data streams > your web stream), then paste it below. It looks like "G-XXXXXXXXXX".
+const GA_ID = 'G-3PR2C4CTBX';
+
+if (GA_ID && !GA_ID.includes('XXXX')) {
+    const s = document.createElement('script');
+    s.async = true;
+    s.src = 'https://www.googletagmanager.com/gtag/js?id=' + GA_ID;
+    document.head.appendChild(s);
+
+    window.dataLayer = window.dataLayer || [];
+    window.gtag = function () { dataLayer.push(arguments); };
+    gtag('js', new Date());
+    gtag('config', GA_ID, {
+        anonymize_ip: true,          // drop the last octet of visitor IPs
+        allow_google_signals: false, // no cross-device/advertising profiles
+        allow_ad_personalization_signals: false
+    });
+}
